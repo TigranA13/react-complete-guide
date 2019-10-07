93c6fe8d1c01e29b300fac18218a3dc8f5c20f36
Tigran Apresyan <tigran.apresyan@betconstruct.com>
2019-10-07 15:55:34 +0400

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
 import Person from './Person/Person';
 
 class App extends Component {
@@ -82,17 +84,17 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>React App</h1>
-        <p className={classes.join(' ')}>This is working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}
-        >
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>React App</h1>
+          <p className={classes.join(' ')}>This is working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}
+          >
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
     );
   }
 }
